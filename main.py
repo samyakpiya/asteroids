e2a428665019e517b8dc3414d9766d86e01252c2
Samyak Piya <samyakpiya@gmail.com>
2025-10-21 12:00:00 -0400

    draw triangle player sprite at the center of screen

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 import pygame
 
-from constants import * 
+from constants import *
+from player import Player
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
     
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+        
         screen.fill("black")
+        
+        player.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
