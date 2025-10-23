822b7b58651dc3bc47816f7c7ba07298d60356d7
Samyak Piya <samyak@budsgunshop.com>
2025-10-22 22:47:09 -0400

    add asteroids spawner

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -14,9 +16,14 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable,)
+
     player = Player(x, y)
+    asteroid_field = AsteroidField()
     
     while True:
         for event in pygame.event.get():
